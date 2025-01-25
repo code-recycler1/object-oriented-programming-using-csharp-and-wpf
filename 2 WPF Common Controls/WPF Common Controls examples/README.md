bd021d272b177c7030f5526cf7cf3f50e3058f6c
r0302772 <dennis.nijs@hotmail.com>
2025-01-25 21:24:34 +0100

    Chapter 2 WPF Common Controls examples

---
+++ 2 WPF Common Controls/WPF Common Controls examples/README.md
@@ -0,0 +1,10 @@
+# WPF Common Controls examples
+
+This project demonstrates the usage of common controls in a WPF application. It includes examples of how to use radio buttons, checkboxes, and labels to create a simple user interface.
+
+- Change the background color of a label using radio buttons.
+- Toggle bold and italic font styles using checkboxes.
+
+![Example 1 Image 1](./Screenshots/Example_1_Situation_1.png)
+
+![Example 1 Image 2](./Screenshots/Example_1_Situation_2.png)
---
+++ 2 WPF Common Controls/WPF Common Controls examples/WPF Common Controls examples/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WPF_Common_Controls_examples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rbRed_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.Background = Brushes.Red;
+            lblResult.Foreground = Brushes.White;
+        }
+
+        private void rbBlue_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.Background = Brushes.Blue;
+            lblResult.Foreground = Brushes.White;
+        }
+
+        private void rbGreen_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.Background = Brushes.Green;
+            lblResult.Foreground = Brushes.White;
+        }
+        private void rbWhite_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.Background = Brushes.White;
+            lblResult.Foreground = Brushes.Black;
+        }
+
+        private void cbBold_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.FontWeight = FontWeights.Bold;
+        }
+
+        private void cbBold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lblResult.FontWeight = FontWeights.Normal;
+        }
+
+        private void cbItalic_Checked(object sender, RoutedEventArgs e)
+        {
+            lblResult.FontStyle = FontStyles.Italic;
+        }
+
+        private void cbItalic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lblResult.FontStyle = FontStyles.Normal;
+        }
+
+    }
+}
